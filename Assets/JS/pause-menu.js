ef2434653002f53869430df685b17b2eab7cf676
gavvahar <nihargavva@gmail.com>
2022-07-26 23:33:16 -0400

    changing the pause button to reset button

--- Assets/JS/pause-menu.js
+++ Assets/JS/pause-menu.js
@@ -1,20 +1,6 @@
-function pause() {
-  var pause = document.getElementById("pause");
-  pause.style.display = "block";
-  var control = document.getElementById("control");
-  control.style.display = "none";
-}
-
 function reset() {
   var pause = document.getElementById("pause");
   pause.style.display = "none";
   init();
   ourStorage.clear();
-}
-
-function resume() {
-  var pause = document.getElementById("pause");
-  pause.style.display = "none";
-  var control = document.getElementById("control");
-  control.style.display = "block";
 }
